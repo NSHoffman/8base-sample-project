2765189107fdec554f5939052c9841de406b9f40
Nikita Zuev <hoffmanmyst@gmail.com>
2021-04-05 12:33:05 +0300

    Updated error handling in ProductThumbnail

--- client/src/routes/products/ProductThumbnail.js
+++ client/src/routes/products/ProductThumbnail.js
@@ -6,6 +6,7 @@ const { REACT_APP_8BASE_API_ENDPOINT: workspace } = process.env;
 export class ProductThumbnail extends React.Component {
   state = {
     default: '',
+    errors: null,
   };
 
   async componentDidMount() {
@@ -16,8 +17,14 @@ export class ProductThumbnail extends React.Component {
 
   setDefaultImage = async query => {
     const formattedQuery = query.replace(/\s/g, '_');
-    const { image } = await fetch(`${workspace}/webhook/${formattedQuery}/get-product-image`).then(res => res.json());
-    this.setState({ default: image });
+    const { image, errors } = await fetch(`${workspace}/webhook/${formattedQuery}/get-product-image`).then(res => res.json());
+
+    if (errors.length) {
+      this.setState({ errors });
+    } 
+    else {
+      this.setState({ default: image });
+    }
   };
 
   render() {
@@ -25,9 +32,14 @@ export class ProductThumbnail extends React.Component {
 
     if (this.props.src) {
       return <img src={this.props.src} alt={`${this.props.query} thumbnail`} style={style} />;
-    } else if (this.state.default) {
+    } 
+    else if (this.state.default) {
       return <img src={this.state.default} alt={`${this.props.query} thumbnail`} style={style} />;
-    } else {
+    }
+    else if (this.state.errors) {
+      return 'Not Available';
+    } 
+    else {
       return <Loader />;
     }
   }
